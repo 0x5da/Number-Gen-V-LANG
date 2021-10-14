b52a9b2b925b7cff960567c013cc46101936ebf1
0x1C64 <70007718+0x1C64@users.noreply.github.com>
2021-10-14 22:29:52 +0100

    Add files via upload

---
+++ rand.v
@@ -0,0 +1,15 @@
+/* 
+just making random stuff in V-Lang, mainly basic stuff
+tryna expand my V knowledge
+starting off with a Number generator in V-Lang
+only 5 lines of code nothing special
+slowly gonna work my way up to better projects
+but for now thisis officially V-LANG project #1
+*/
+
+import rand
+
+fn main() {
+  num := rand.int_in_range(1, 1000)
+  print(num)
+}
